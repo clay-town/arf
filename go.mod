7c10311729b2ea7c2626d380f250d8d1de9650a4
Natashia White <whitenat@oregonstate.edu>
2020-03-21 13:22:02 -0700

    add form elements and js to pull their contents upon submit references #1

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/clay-town/zamfam-true
 
 go 1.13
+
+require github.com/joho/godotenv v1.3.0 // indirect
--- internal/ui/static/restful.js
+++ internal/ui/static/restful.js
@@ -1,10 +1,36 @@
 document.addEventListener("DOMContentLoaded", function() {
+  createApplication();
   //statusCheck();
 });
 
+
 function createApplication(){
+  // var x = document.getElementById("initial_nv");
+  // var text = "";
+  // var i;
+  // for (i = 0; i < x.length ;i++) {
+  //   text += x.elements[i].value + "<br>";
+  // }
+  button = document.getElementById("nv_submit");
+  button.addEventListener("click", function(){
+  benefit=document.getElementById("benefits").value;
+  fname = document.getElementById("fname").value;
+  lname = document.getElementById("lname").value;
+  dob = document.getElementById("dob").value;
+  tribalID = document.getElementById("tribalID").value;
+  address = document.getElementById("address").value;
+  zip = document.getElementById("zip").value;
+  ssn = document.getElementById("ssn").value;
+  console.log(benefit);
+  console.log(fname);
+  console.log(lname);
+  console.log(dob);
+  console.log(tribalID);
+  console.log(address);
+  console.log(zip);
+  console.log(ssn);
   // grab your vars
-  
+  });
 }
 
 function getUrlVars() {
@@ -29,6 +55,10 @@ function statusCheck(eligibilityCheckId = "62265B7E2DCFF1B7C9A1A47890A72E72E02F6
     }
     request.send();
 }
+
+// document.getElementById("nv_submit").addEventListener("click", function() {
+//     alert("Hello World!");
+//   });
 
 // function viaJavaScript(eligibilityCheckId = "62265B7E2DCFF1B7C9A1A47890A72E72E02F6341DED39B6784CC11D071182F3A") {
 //   var data = {};
