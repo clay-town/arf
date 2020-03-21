cfcaae076ce5fba7035ea842d4b75657a44b9096
Natashia White <whitenat@oregonstate.edu>
2020-03-21 13:23:59 -0700

    add form elements and js to pull their contents upon submit references #1

--- internal/ui/static/restful.js
+++ internal/ui/static/restful.js
@@ -5,31 +5,25 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 function createApplication(){
-  // var x = document.getElementById("initial_nv");
-  // var text = "";
-  // var i;
-  // for (i = 0; i < x.length ;i++) {
-  //   text += x.elements[i].value + "<br>";
-  // }
   button = document.getElementById("nv_submit");
+  
   button.addEventListener("click", function(){
-  benefit=document.getElementById("benefits").value;
-  fname = document.getElementById("fname").value;
-  lname = document.getElementById("lname").value;
-  dob = document.getElementById("dob").value;
-  tribalID = document.getElementById("tribalID").value;
-  address = document.getElementById("address").value;
-  zip = document.getElementById("zip").value;
-  ssn = document.getElementById("ssn").value;
-  console.log(benefit);
-  console.log(fname);
-  console.log(lname);
-  console.log(dob);
-  console.log(tribalID);
-  console.log(address);
-  console.log(zip);
-  console.log(ssn);
-  // grab your vars
+    benefit=document.getElementById("benefits").value;
+    fname = document.getElementById("fname").value;
+    lname = document.getElementById("lname").value;
+    dob = document.getElementById("dob").value;
+    tribalID = document.getElementById("tribalID").value;
+    address = document.getElementById("address").value;
+    zip = document.getElementById("zip").value;
+    ssn = document.getElementById("ssn").value;
+    console.log(benefit);
+    console.log(fname);
+    console.log(lname);
+    console.log(dob);
+    console.log(tribalID);
+    console.log(address);
+    console.log(zip);
+    console.log(ssn);
   });
 }
 
