69332ff6ef97e600e8d2ec3ce0f5e534cabc03a2
Natashia White <whitenat@oregonstate.edu>
2020-03-21 16:28:46 -0700

    add post request function references 01_create_application_form

--- cmd/zamfamtrue/handlers.go
+++ cmd/zamfamtrue/handlers.go
@@ -9,6 +9,8 @@ import (
 	"io/ioutil"
 	"os"
 	"github.com/joho/godotenv"
+	"fmt"
+	"strings"
 )
 
 func createApplication(w http.ResponseWriter, r *http.Request) {
@@ -26,11 +28,35 @@ func createApplication(w http.ResponseWriter, r *http.Request) {
 	zip := r.URL.Query().Get("zip");
 	ssn := r.URL.Query().Get("ssn");
 
-	APIKEY := os.Getenv("APIKEY");
+	// APIKEY := os.Getenv("APIKEY");
 
-	client := &http.Client{
+	// client := &http.Client{
 
+	// }
+
+
+	url := "https://api.universalservice.org/nvca-svc/consumer/eligibility-check"
+	method := "PUT"
+
+	payload := strings.NewReader("{\n\"firstName\": \"JOHN\",\n\"middleName\": \"APPLESAUCE\",\n\"lastName\": \"SMITH\",\n\"address\": \"12345 MADE UP LANE\",\n\"state\": \"MD\",\n\"city\": \"LOS CANADA\",\n\"zipCode\": \"00234\",\n\"urbanizationCode\": \"URB ROYAL OAKS\",\n\"dob\": \"1970-11-23\",\n\"ssn4\": \"0123\",\n\"tribalId\": \"46631\",\n\"bqpFirstName\": \"JOHN\",\n\"bqpLastName\": \"DOE\",\n\"bqpDob\": \"1990-09-09\",\n\"bqpSsn4\": \"4567\",\n\"bqpTribalId\": \"899939\",\n\"eligibilityProgramCode\": \"E1,E2,E3\",\n\"consentInd\": \"Y\",\n\"contactPhoneNumber\": \"9999999999\",\n\"contactEmail\": \"JOHNSMITH@EMAIL.COM\",\n\"contactAddress\": \"123 MADE UP LANE\",\n\"contactCity\": \"NEW YORK\",\n\"contactState\": \"NY\",\n\"contactZipCode\": \"10038\",\n\"contactUrbCode\": \"URB ROYAL OAKS\",\n\"repId\": \"\",\n\"repNotAssisted\": \"1\",\n\"carrierUrl\": \"companysite.com\"\n}")
+
+	client := &http.Client {
 	}
+	req, err := http.NewRequest(method, url, payload)
+
+	if err != nil {
+	fmt.Println(err)
+	}
+	req.Header.Add("Content-Type:", "application/json")
+	req.Header.Add("authorization", "Basic ODdAbGhnOWN3NHRneDlqN2JqLnVxZm9kenlwenRybnA6eVVfKWFfMlNyJGpLbEgtXzRzXkBRVGJpTE45KV5GRno=")
+	req.Header.Add("accept-language", "en-US,en;q=0.8")
+	req.Header.Add("content-type", "application/json")
+
+	res, err := client.Do(req)
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+
+	fmt.Println(string(body))
 }
 
 func statusCheck(w http.ResponseWriter, r *http.Request) {
--- internal/ui/static/restful.js
+++ internal/ui/static/restful.js
@@ -23,7 +23,7 @@ function createApplication(){
 
     var request = new XMLHttpRequest();
     console.log("create app js function");
-    var url = "/createApplication?benefit="+benefit+"fname="+fname+"lname="+lname+"dob="+dob+"trabalID="+trabalID+"address="
+    var url = "/createApplication?benefit="+benefit+"fname="+fname+"lname="+lname+"dob="+dob+"tribalID="+tribalID+"address="
     +address+"zip="+zip+"ssn="+ssn;
 
     request.open('POST', url, true);
