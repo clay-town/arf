5f2c74d0ff41cbb9f0e5fe23f2cdac560001c71c
clay-town <justinmccune@icloud.com>
2020-03-23 12:16:13 -0700

    try again

--- internal/ui/static/restful.js
+++ internal/ui/static/restful.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log('IMAGE:',base64Img);
   })
  */
-function convertImgToBase64(url){
+function convertImgToBase64(url, callback, outputFormat){
   var canvas = document.createElement('CANVAS');
   var ctx = canvas.getContext('2d');
   var img = new Image;
@@ -28,30 +28,33 @@ function convertImgToBase64(url){
     canvas.height = img.height;
     canvas.width = img.width;
       ctx.drawImage(img,0,0);
-      var dataURL = canvas.toDataURL('image/png');
-      console.log("convertImgToBase64 anon function");
-      console.log(data)
-            //   $('.output')
-            // .find('textarea')
-            //     .val(dataURL)
-            //     .end()
-            // .find('a')
-            //     .attr('href', dataURL)
-            //     .text(dataURL)
-            //     .end()
-            // .find('img')
-            //     .attr('src', dataURL);
+      var dataURL = canvas.toDataURL(outputFormat || 'image/png');
+      callback.call(this, dataURL);
+        // Clean up
       canvas = null; 
   };
   img.src = url;
 }
 
-// $('#img2b64').submit(function(event){
-//     var imageUrl = $(this).find('input[name=url]').val();
-//     console.log('imageUrl', imageUrl);
-//     convertImgToBase64(imageUrl);
-//     event.preventDefault();
-// });
+
+$('#img2b64').submit(function(event){
+    var imageUrl = $(this).find('input[name=url]').val();
+    console.log('imageUrl', imageUrl);
+    convertImgToBase64(imageUrl, function(base64Img){
+        $('.output')
+            .find('textarea')
+                .val(base64Img)
+                .end()
+            .find('a')
+                .attr('href', base64Img)
+                .text(base64Img)
+                .end()
+            .find('img')
+                .attr('src', base64Img);
+    });
+    
+    event.preventDefault();
+});
 
 
 function uploadProof() {
