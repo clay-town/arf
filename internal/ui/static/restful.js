9755462fc8c030e24ac22518eef5d70414d39b48
clay-town <justinmccune@icloud.com>
2020-03-23 12:28:34 -0700

    remove $

--- internal/ui/static/restful.js
+++ internal/ui/static/restful.js
@@ -19,7 +19,8 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log('IMAGE:',base64Img);
   })
  */
-function convertImgToBase64(url, callback, outputFormat){
+ 
+function convertImgToBase64(url){
   var canvas = document.createElement('CANVAS');
   var ctx = canvas.getContext('2d');
   var img = new Image;
@@ -28,33 +29,14 @@ function convertImgToBase64(url, callback, outputFormat){
     canvas.height = img.height;
     canvas.width = img.width;
       ctx.drawImage(img,0,0);
-      var dataURL = canvas.toDataURL(outputFormat || 'image/png');
-      callback.call(this, dataURL);
+      var dataURL = canvas.toDataURL('image/png');
+      console.log(dataURL)
+      //callback.call(this, dataURL);
         // Clean up
       canvas = null; 
   };
   img.src = url;
 }
-
-
-$('#img2b64').submit(function(event){
-    var imageUrl = $(this).find('input[name=url]').val();
-    console.log('imageUrl', imageUrl);
-    convertImgToBase64(imageUrl, function(base64Img){
-        $('.output')
-            .find('textarea')
-                .val(base64Img)
-                .end()
-            .find('a')
-                .attr('href', base64Img)
-                .text(base64Img)
-                .end()
-            .find('img')
-                .attr('src', base64Img);
-    });
-    
-    event.preventDefault();
-});
 
 
 function uploadProof() {
@@ -99,7 +81,7 @@ function uploadProof() {
 }
 
 function createCustomer() {
-  button = document.getElementById("vcare_submit");
+  button = document.getElementById("img2b64");
 
   button.addEventListener("click", function(){
     uploadProof()
