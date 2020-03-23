09b33f7d922671abb189a3d6d441de6a998b9758
clay-town <justinmccune@icloud.com>
2020-03-22 22:40:59 -0700

    add zip

--- internal/ui/static/restful.js
+++ internal/ui/static/restful.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
   statusCheck();
   submitApplication();
   sendUserToManyChatFlowAfterApplicationCreation();
+  checkServiceAvailability()
 });
 
 function checkServiceAvailability(){
@@ -10,19 +11,31 @@ function checkServiceAvailability(){
   button = document.getElementById("check_sevice_submit");
 
   button.addEventListener("click", function(){
-    var request = new XMLHttpRequest();
     console.log("check_service js function");
-    
+    var zipCode = 
+    var request = new XMLHttpRequest();
     var url = "/checkservice";
+    var zipCode = 
 
     request.open('POST', url, true);
     request.onload = function(){
       var data = this.response;
-      console.log(data);
       obj=JSON.parse(data)
-      //document.getElementById("check_service").innerHTML = "check_service: " + JSON.parse(obj).message;
-      console.log(JSON.parse(data));
+      parser = new DOMParser();
+
+      xmlDoc = parser.parseFromString(obj,"text/xml");
+
+      description = xmlDoc.getElementsByTagName("description")[0].innerHTML
+      enrollmentId = xmlDoc.getElementsByTagName("enrollmentId")[0].innerHTML
+
+      document.getElementById("check_service").innerHTML = "Description: " + description
+      // change this to populate the enrollment_id directly into the form after it's made
+      document.getElementById("enrollment_id").innerHTML = "Enrollment Id: " + enrollmentId
       
+
+      console.log(obj)
+      console.log(xmlDoc.getElementsByTagName("description"))
+      console.log(xmlDoc.getElementsByTagName("description")[0].innerHTML)     
       }
       request.send();
 
