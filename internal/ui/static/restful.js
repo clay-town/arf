6e059a4732674eeada9fc0d374e3d7737524622d
clay-town <justinmccune@icloud.com>
2020-03-23 17:47:51 -0700

    extra shit

--- internal/ui/static/restful.js
+++ internal/ui/static/restful.js
@@ -60,6 +60,7 @@ function uploadProof() {
     var enrollmentId = document.getElementById("enrollment_id_b").value;
     const wait=ms=>new Promise(resolve => setTimeout(resolve, ms)); 
 
+    console.log("deeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeebug")
     convertImgToBase64(additionalProof, "ap");
     convertImgToBase64(photoId, "id");
     convertImgToBase64(pob, "pob");    
@@ -459,33 +460,6 @@ function clearFields(){
     document.getElementById('status_form').reset();
     document.getElementById('submit_vcare_form').reset();
   });
-}
-
-/**
- * convertImgToBase64
- * @param  {String}   url
- * @param  {Function} callback
- * @param  {String}   [outputFormat='image/png']
- * @author HaNdTriX
- * @example
-  convertImgToBase64('http://goo.gl/AOxHAL', function(base64Img){
-    console.log('IMAGE:',base64Img);
-  })
- */
-function convertImgToBase64(url, callback, outputFormat){
-  var canvas = document.createElement('CANVAS');
-  var ctx = canvas.getContext('2d');
-  var img = new Image;
-  img.crossOrigin = 'Anonymous';
-  img.onload = function(){
-    canvas.height = img.height;
-    canvas.width = img.width;
-      ctx.drawImage(img,0,0);
-      var dataURL = canvas.toDataURL(outputFormat || 'image/png');
-      //console.log(dataURL);
-      document.getElementById("status").innerHTML = dataURL;
-    return(dataURL);
-  };
 }
 
 function submitApplication(){
