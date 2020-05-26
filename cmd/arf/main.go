be06f26622b94a3c0361bcdb04aaf4e3cf7a2315
clay-town <justinmccune@icloud.com>
2020-05-26 15:56:19 -0700

    building

--- cmd/arf/main.go
+++ cmd/arf/main.go
@@ -1,14 +1,14 @@
 package main 
 
 import (
-	// i "arf"
+	i "github.com/clay-town/arf/internal/arf"
 	"log"
 	"net/http"
 	"os"
 	"github.com/joho/godotenv"
 )
 
-// var items i.Items
+var items i.Items
 
 func main() {
 	fs := http.FileServer(http.Dir("./internal/ui/static/"))
